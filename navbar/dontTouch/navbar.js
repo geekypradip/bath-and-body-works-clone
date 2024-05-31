a28871dba3c38218bd551d76e0551c2885d213f8
pradip mandal <join2me.pm@gmail.com>
2024-05-31 19:32:13 +0530

    Refactor navbar.js code

--- navbar/dontTouch/navbar.js
+++ navbar/dontTouch/navbar.js
@@ -1,129 +1,128 @@
-document.getElementById('cartclick').onclick=()=>{
- window.location.href="./cart/cart.html";
-}
-let n=(v)=>{
-   let store= document.querySelectorAll('.newAndNow .child')
-   for(let i in store){
-       if(i==v) store[v].style.display="block";
-       else store[i].style.display="none";
-   }
-    
-}
-let b=(v)=>{
-  let store=  document.querySelectorAll('.bodycare .child')
-  for(let i in store){
-    if(i==v) store[v].style.display="block";
-    else store[i].style.display="none";
-}
-}
-let h=(v)=>{
-   let store= document.querySelectorAll('.handSoapSanatizer .child')
-   for(let i in store){
-    if(i==v) store[v].style.display="block";
-    else store[i].style.display="none";
-}
-}
-  let ho=(v)=>{
-     let store=  document.querySelectorAll('.homeFragrance .child')
-     for(let i in store){
-        if(i==v) store[v].style.display="block";
-        else store[i].style.display="none";
-    }
-  }  
-   let g=(v)=>{
+document.getElementById("cartclick").onclick = () => {
+  window.location.href = "./cart/cart.html";
+};
+let n = (v) => {
+  let store = document.querySelectorAll(".newAndNow .child");
+  for (let i in store) {
+    if (i == v) store[v].style.display = "block";
+    else store[i].style.display = "none";
+  }
+};
+let b = (v) => {
+  let store = document.querySelectorAll(".bodycare .child");
+  for (let i in store) {
+    if (i == v) store[v].style.display = "block";
+    else store[i].style.display = "none";
+  }
+};
+let h = (v) => {
+  let store = document.querySelectorAll(".handSoapSanatizer .child");
+  for (let i in store) {
+    if (i == v) store[v].style.display = "block";
+    else store[i].style.display = "none";
+  }
+};
+let ho = (v) => {
+  let store = document.querySelectorAll(".homeFragrance .child");
+  for (let i in store) {
+    if (i == v) store[v].style.display = "block";
+    else store[i].style.display = "none";
+  }
+};
+let g = (v) => {
+  let store = document.querySelectorAll(".gift .child");
+  for (let i in store) {
+    if (i == v) store[v].style.display = "block";
+    else store[i].style.display = "none";
+  }
+};
 
-   let store= document.querySelectorAll('.gift .child')
-   for(let i in store){
-    if(i==v) store[v].style.display="block";
-    else store[i].style.display="none";
-}
-   }
+let patanehi = (v, e) => {
+  let l = document.querySelectorAll(".link");
+  for (let i = 0; i < l.length; i++) {
+    l[i].style.borderBottom = "none";
+  }
+  console.log(l);
+  e.style.borderBottom = "3px solid blue";
+  let store = document.querySelectorAll(".pp");
+  // let dropdown= document.querySelectorAll('.dropdown')
+  if (v === "empty") {
+    for (let i in store) {
+      store[i].style.display = "none";
+    }
+  }
 
-let patanehi=(v,e)=>{
-    let l=document.querySelectorAll('.link')
-    for(let i=0;i<l.length;i++){
-        l[i].style.borderBottom="none";
+  for (let i in store) {
+    if (i == v) {
+      store[v].style.display = "block";
+      store[v].style.display = "flex";
+    } else {
+      store[i].style.display = "none";
     }
-    console.log(l)
-    e.style.borderBottom="3px solid blue";
-    let store= document.querySelectorAll('.pp');
-    // let dropdown= document.querySelectorAll('.dropdown')
-    if(v==="empty"){
-        for(let i in store){
-           
-                store[i].style.display="none";
-                
-             }
-        }
-    
-    for(let i in store){
-        if(i==v){store[v].style.display="block";store[v].style.display="flex"} 
-        else {
-            store[i].style.display="none";
-            
-         }
-    }
-}
+  }
+};
 var tog = document.getElementById("tog");
 var count = 1;
-        const fnDe = async ()=>{
-            let input = tog.value;
-            let data = await fetch(`http://localhost:3000/product?q=${input}&_limit=4&_page=${1}`)
-            let res = await data.json();
-            console.log(res)
-            createDeb(res)
-            // console.log(product)
-        }
-        function deBouncer(fn,delay){
-            let id;
-            return ()=>{
-                id && clearTimeout(id);
-                id = setTimeout(()=>fn(),delay);
-            }
-        }
-        function createDeb(data){
-            let dis = document.getElementById("dis");
-            dis.innerHTML = "";
-            // let nextBtn = document.createElement("button");
-            // nextBtn.innerText = ">>";
-            // nextBtn.className = "next";
-            for(let id of data){
-                let div = document.createElement("div");
-                let img = document.createElement("img");
-                img.src = id.productImage;
-                let span2 = document.createElement("span");
-                let span = document.createElement("span");
-                span.innerText = id.name;
-                let span1 = document.createElement("span");
-                span1.innerText = `$${id.price}`;
-                span1.className = "amt";
-                span2.append(span,span1)
-                let span3 = document.createElement("span");
-                let button = document.createElement("button");
-                button.onclick=()=>{
-                    // alert("got it")
-                    localStorage.setItem("productId",id.id)
-                    window.location.href = "/hustle/viewProduct.html"
-                }
-                button.innerText = "View Product"
-                span3.innerHTML = `<i class="material-icons">favorite</i>`
-                div.append(img,span2,span3,button)
-                dis.append(div)
-            }
-            dis.style.display = "flex";
-            // dis.append(nextBtn)
-        }
-        tog.addEventListener("keydown",deBouncer(fnDe,1000))
-        function check() {
-            let display = document.getElementById("dis");
-            if( tog.value == ""){
-                display.style.display = "none";
-            }
-        }
-        const funCheck = setInterval(check,500)
-        window.addEventListener("load",funCheck)
+const fnDe = async () => {
+  let input = tog.value;
+  let data = await fetch(
+    `http://localhost:3000/product?q=${input}&_limit=4&_page=${1}`
+  );
+  let res = await data.json();
+  console.log(res);
+  createDeb(res);
+  // console.log(product)
+};
+function deBouncer(fn, delay) {
+  let id;
+  return () => {
+    id && clearTimeout(id);
+    id = setTimeout(() => fn(), delay);
+  };
+}
+function createDeb(data) {
+  let dis = document.getElementById("dis");
+  dis.innerHTML = "";
+  // let nextBtn = document.createElement("button");
+  // nextBtn.innerText = ">>";
+  // nextBtn.className = "next";
+  for (let id of data) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = id.productImage;
+    let span2 = document.createElement("span");
+    let span = document.createElement("span");
+    span.innerText = id.name;
+    let span1 = document.createElement("span");
+    span1.innerText = `$${id.price}`;
+    span1.className = "amt";
+    span2.append(span, span1);
+    let span3 = document.createElement("span");
+    let button = document.createElement("button");
+    button.onclick = () => {
+      // alert("got it")
+      localStorage.setItem("productId", id.id);
+      window.location.href = "/viewProduct.html";
+    };
+    button.innerText = "View Product";
+    span3.innerHTML = `<i class="material-icons">favorite</i>`;
+    div.append(img, span2, span3, button);
+    dis.append(div);
+  }
+  dis.style.display = "flex";
+  // dis.append(nextBtn)
+}
+tog.addEventListener("keydown", deBouncer(fnDe, 1000));
+function check() {
+  let display = document.getElementById("dis");
+  if (tog.value == "") {
+    display.style.display = "none";
+  }
+}
+const funCheck = setInterval(check, 500);
+window.addEventListener("load", funCheck);
 // let dis=(e)=>{
-//     document.querySelectorAll('.child')[e] 
+//     document.querySelectorAll('.child')[e]
 // }
-//                                    
-// 
+//
+//
--- navbar/integratenav.js
+++ navbar/integratenav.js
@@ -1,10 +1,12 @@
-let link=document.createElement('link');
-link.rel="stylesheet";link.href="navbar/dontTouch/nav.css"
-let ilink=document.createElement('link');
-ilink.rel="stylesheet";ilink.href="https://fonts.googleapis.com/icon?family=Material+Icons"
-document.head.append(link,ilink)
-let content=document.createElement('div');
-content.innerHTML=`<div class="nav">
+let link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "navbar/dontTouch/nav.css";
+let ilink = document.createElement("link");
+ilink.rel = "stylesheet";
+ilink.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+document.head.append(link, ilink);
+let content = document.createElement("div");
+content.innerHTML = `<div class="nav">
 <div class="empty"></div>
 <div class="logo"> <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.17f/on/demandware.static/-/Sites-BathAndBodyWorks-Library/en_US/v1635414384794/images/Christmas2021/christmas_logo_site.svg?yocs=o_s_" alt="Logo" id="logo"></div>
 
@@ -23,34 +25,34 @@ content.innerHTML=`<div class="nav">
 </div>
 <div class="header">
 <div class="dropdown" data-dropdown>
-     
-<div class="link" onmouseover="patanehi(0,this)"><a href="/hustle/home.html">NEW & NOW</a></div>
+
+<div class="link" onmouseover="patanehi(0,this)"><a href="/">NEW & NOW</a></div>
 
 </div>
 
 <div class="dropdown" data-dropdown>
 
-<div class="link" onmouseover="patanehi(1,this)"><a href="/hustle/bodycare.html">BODY CARE</a></div>
+<div class="link" onmouseover="patanehi(1,this)"><a href="/bodycare.html">BODY CARE</a></div>
 
 </div>
 
 
 <div class="dropdown" data-dropdown>
-<div class="link" onmouseover="patanehi(2,this)"><a href="/hustle/handSoapSanatizer.html"> HAND SOAPS $ SANITIZERS</a></div>
+<div class="link" onmouseover="patanehi(2,this)"><a href="/handSoapSanatizer.html"> HAND SOAPS $ SANITIZERS</a></div>
 </div>
 <div class="dropdown" data-dropdown>
 
-<div class="link" onmouseover="patanehi(3,this)"><a href="/hustle/bodycare.html"> HOME FRAGRANCE </a></div>
+<div class="link" onmouseover="patanehi(3,this)"><a href="/bodycare.html"> HOME FRAGRANCE </a></div>
 
 </div>
 
 
 <div class="dropdown" data-dropdown>
-<div class="link" onmouseover="patanehi(4,this)"><a href="/hustle/gifts.html">GIFT</a></div>
+<div class="link" onmouseover="patanehi(4,this)"><a href="/gifts.html">GIFT</a></div>
 </div>
 
 <div class="dropdown" data-dropdown>
-<div class="link" onmouseover="patanehi('empty',this)"><a href="/hustle/gifts.html">TOP OFFERS</a></div>
+<div class="link" onmouseover="patanehi('empty',this)"><a href="/gifts.html">TOP OFFERS</a></div>
 </div>
 </div>
 <div class="container">
@@ -62,20 +64,20 @@ content.innerHTML=`<div class="nav">
     <div class="childs">
         <div class="child">
             <div>NEW & NOW</div>
-           <div>New Arrivals</div> 
-            <div>Christmas Collection</div> 
-           <div>Best Sellers</div> 
-           <div>Aromatherapy Shop</div> 
+           <div>New Arrivals</div>
+            <div>Christmas Collection</div>
+           <div>Best Sellers</div>
+           <div>Aromatherapy Shop</div>
              <div>Men's Shop</div>
      </div>
-    
+
     </div>
     <div class="static">
             <div>get inpired</div>
             <div>shop by fragrance</div>
-    </div> 
+    </div>
  </div>
-   <!-- djjdjdjd  --> 
+   <!-- djjdjdjd  -->
    <!-- <hr> -->
    <div class="bodycare pp">
     <div class="parent">
@@ -91,53 +93,53 @@ content.innerHTML=`<div class="nav">
     <div class="childs">
         <div class="child">
          <div>NEW Body  care</div>
-          <div>Customer Fabourites</div> 
-         <div>Retired Fragrance</div> 
-         <div>Travel Size </div>  
+          <div>Customer Fabourites</div>
+         <div>Retired Fragrance</div>
+         <div>Travel Size </div>
     </div>
     <div class="child">
         <div>All bath & shower </div>
          <div>Body wash & shower gel </div>
-        <div>Body Scrub</div> 
+        <div>Body Scrub</div>
         <div>Bar soap </div>
         <div>Shampoo & Conditioner</div>
-        <div>bath Accessories</div>  
+        <div>bath Accessories</div>
     </div>
     <div class="child">
         <div>All Moisturizers</div>
-         <div>Body Lotion</div> 
-        <div>Body Cream</div> 
-        <div>Hand Cream</div> 
-        <div>lip Gloss & Balms</div> 
+         <div>Body Lotion</div>
+        <div>Body Cream</div>
+        <div>Hand Cream</div>
+        <div>lip Gloss & Balms</div>
     </div>
     <div class="child">
         <div>All Fragrance</div>
-         <div>Body Sprays & Mists </div> 
-        <div>Perfume & Cologne</div>   
+         <div>Body Sprays & Mists </div>
+        <div>Perfume & Cologne</div>
     </div>
     <div class="child">
         <div>All Aromatherapy</div>
-         <div>Body wash & shower gel</div> 
-        <div>Body Scrub</div> 
+         <div>Body wash & shower gel</div>
+        <div>Body Scrub</div>
         <div>Moisturizers</div>
-        <div>Mists & Sprays</div>  
+        <div>Mists & Sprays</div>
     </div>
     <div class="child">
         <div>All men's</div>
-         <div>Body wash & shower gel</div> 
-        <div>Moisturizers</div> 
-        <div>Body Sprays & cologne</div>  
+         <div>Body wash & shower gel</div>
+        <div>Moisturizers</div>
+        <div>Body Sprays & cologne</div>
     </div>
     <div class="child">
-        <div>Body Care Gift Sets</div>  
+        <div>Body Care Gift Sets</div>
     </div>
     </div>
-    
+
     <div class="static">
         <div>BODY CARE OFFERS</div>
         <div>Buy 3,Get 3 Free Selct Body Care </div>
         <div>Buy 2 Get 1 Free Travel, Hand & Lip Care</div>
-    </div> 
+    </div>
    </div>
    <!-- <hr>   ggjjgjgjj -->
    <!-- <hr> -->
@@ -152,33 +154,33 @@ content.innerHTML=`<div class="nav">
     <div class="childs">
         <div class="child">
          <div>NEW Hand soap & sanitizers</div>
-          <div>Gentle & clean hand soaps</div>  
+          <div>Gentle & clean hand soaps</div>
     </div>
     <div class="child">
         <div>All hand soaps</div>
          <div>foaming soaps  </div>
-        <div>gel & nourshing soaps </div> 
+        <div>gel & nourshing soaps </div>
         <div>soap holder</div>
     </div>
     <div class="child">
         <div>All hand sanitizers</div>
-         <div>pocketbac sanitizers</div> 
-        <div>sanitizers Sprays</div> 
-        <div>Full-size sanitizers</div> 
-        <div>Hand sanitizers holders</div> 
+         <div>pocketbac sanitizers</div>
+        <div>sanitizers Sprays</div>
+        <div>Full-size sanitizers</div>
+        <div>Hand sanitizers holders</div>
     </div>
     <div class="child">
-        <div>hand soap & sanitizers gift sets </div>   
+        <div>hand soap & sanitizers gift sets </div>
     </div>
-    
+
     </div>
-    
+
     <div class="static">
         <div>HAND SOAPS & SANITIZERS  OFFERS</div>
         <div>6/$27 or 4/$20 hand soaps </div>
         <div>$8 hand sanitizers 5-packs</div>
         <div>3/$1 hand sanitizers sprays</div>
-    </div> 
+    </div>
    </div>
     <!-- <hr>   ggjjgjgjj -->
     <!-- <hr> -->
@@ -193,33 +195,33 @@ content.innerHTML=`<div class="nav">
      <div class="childs">
          <div class="child">
           <div>NEW Home fragrance</div>
-           <div>The white barn shop</div>  
+           <div>The white barn shop</div>
      </div>
      <div class="child">
          <div>All candles</div>
           <div>3-wick candles</div>
-         <div>single wick candles </div> 
+         <div>single wick candles </div>
          <div>candle holder & Accessories</div>
      </div>
      <div class="child">
          <div>All wallflowers</div>
-          <div>wallflowers plugs</div> 
-         <div>wallflowers refillis</div> 
-         <div>room spray & Mists</div> 
-         <div>car fragrance</div> 
+          <div>wallflowers plugs</div>
+         <div>wallflowers refillis</div>
+         <div>room spray & Mists</div>
+         <div>car fragrance</div>
      </div>
      <div class="child">
-         <div>candle gift sets </div>   
+         <div>candle gift sets </div>
      </div>
-     
+
      </div>
-     
+
      <div class="static">
          <div>HOME FRAGRANCE  OFFERS</div>
          <div>$5.96 all single wick candles</div>
          <div>$8 wallflowers</div>
          <div>3/$1 room spray</div>
-     </div> 
+     </div>
     </div>
      <!-- <hr>   ggjjgjgjj -->
    <!-- <hr> -->
@@ -236,22 +238,22 @@ content.innerHTML=`<div class="nav">
         <div class="child">
          <div>christmas gift guide</div>
           <div>gift sets</div>
-          <div>stocking stuffers</div>  
+          <div>stocking stuffers</div>
     </div>
     <div class="child">
         <div>$15 & under gifts</div>
          <div>$30 & under gifts</div>
-        <div>$50 & under gifts</div> 
+        <div>$50 & under gifts</div>
     </div>
     <div class="child">
         <div>gift for her</div>
-         <div>gift for him</div> 
-        <div>gift for everyone</div> 
-        <div>gifts for the hostess</div>  
+         <div>gift for him</div>
+        <div>gift for everyone</div>
+        <div>gifts for the hostess</div>
     </div>
     <div class="child">
         <div>gift boxes & bags</div>
-        <div>accessories</div>   
+        <div>accessories</div>
     </div>
     <div class="child">
         <div>E-gift cards</div>
@@ -262,9 +264,9 @@ content.innerHTML=`<div class="nav">
    </div>
 </div>
 <div id="dis">
-</div>`
+</div>`;
 
-document.body.insertBefore(content,document.body.firstChild);
-let script= document.createElement('script');
-script.src="navbar/dontTouch/navbar.js";
-document.body.append(script)
+document.body.insertBefore(content, document.body.firstChild);
+let script = document.createElement("script");
+script.src = "navbar/dontTouch/navbar.js";
+document.body.append(script);
